2f810e43cdab5a26e198ee22f978fdb15d4c5ba0
Dinh-Van Colomban <van.colomban@gmail.com>
2025-04-21 19:33:52 +0200

    feat(svelte): adopt more recommended rules

--- lib/configs/svelte.config.ts
+++ lib/configs/svelte.config.ts
@@ -12,6 +12,31 @@ export const svelte: OptionsOverrides = {
         prefer: 'double',
       },
     ],
+
+    // eslint-plugin-svelte recommended rules
+    'svelte/infinite-reactive-loop': 'error',
+    'svelte/no-dom-manipulating': 'error',
+    'svelte/no-dupe-on-directives': 'error',
+    'svelte/no-immutable-reactive-statements': 'error',
+    'svelte/no-inspect': 'warn',
+    'svelte/no-raw-special-elements': 'error',
+    'svelte/no-reactive-reassign': 'error',
+    'svelte/no-store-async': 'error',
+    'svelte/no-svelte-internal': 'error',
+    'svelte/no-unnecessary-state-wrap': 'error',
+    'svelte/no-unused-props': 'error',
+    'svelte/no-useless-children-snippet': 'error',
+    'svelte/require-each-key': 'warn',
+    'svelte/require-event-dispatcher-types': 'error',
+    'svelte/require-store-reactive-access': 'error',
+    'svelte/valid-prop-names-in-kit-pages': 'error',
+
+    // Stylistic
+    'svelte/shorthand-attribute': 'error',
+    'svelte/shorthand-directive': 'error',
+    'svelte/first-attribute-linebreak': 'error',
+    'svelte/html-closing-bracket-new-line': 'error',
+    'svelte/html-self-closing': 'error',
   },
 };
 
